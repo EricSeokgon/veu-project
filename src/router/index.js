61266ba33862d1324f1767c04bc3b2323e547583
EricSeokgon <leesk55@gmail.com>
2021-04-20 18:21:29 +0900

    App 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory
+} from 'vue-router'
 import Home from '../views/Home.vue'
+import DataBinding from '../views/DataBinding.vue'
 
 const routes = [
   {
@@ -14,6 +18,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/databinding',
+    name: 'DataBinding',
+    component: DataBinding
   }
 ]
 
