242c302e29ee9c6a3177a1657c05d576a439c9a8
EricSeokgon <leesk55@gmail.com>
2021-04-21 15:45:47 +0900

    데이터 바인딩 테스트

--- src/router/index.js
+++ src/router/index.js
@@ -5,25 +5,23 @@ import {
 import Home from '../views/Home.vue'
 import DataBinding from '../views/DataBinding.vue'
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/databinding',
-    name: 'DataBinding',
-    component: DataBinding
-  }
+const routes = [{
+  path: '/',
+  name: 'Home',
+  component: Home
+},
+{
+  path: '/about',
+  name: 'About',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  component: () => import( /* webpackChunkName: "about" */ '../views/About.vue')
+}, {
+  path: '/databinding',
+  name: 'DataBinding',
+  component: DataBinding
+}
 ]
 
 const router = createRouter({
